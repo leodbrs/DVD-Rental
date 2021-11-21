6ecf2ea8b7a3ccfd7715e822fb1997801058f484
Léo D <81977224+leodbrs@users.noreply.github.com>
2021-11-22 00:01:14 +0100

    Changement d'IP de la DB

--- app/includes/infoConnexion.php
+++ app/includes/infoConnexion.php
@@ -2,5 +2,5 @@
 $user = "root"; // nom utilisateur sur la base de donnée
 $pass = "root"; // mot de passe du l'utilisateur
 $dbname = "rt1projet18"; // nom de la base de donnée
-$lehost = "192.168.91.135"; //adresse du serveur postgresql
+$lehost = "localhost"; //adresse du serveur postgresql
 $leport = "5432"; // port de ce serveur
